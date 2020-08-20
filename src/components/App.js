a65030b53e5e527f5c1be6eb9db803354bbe4a49
Berkay Rahmi Kayaoğlu <berkayrahmikayaoglu@gmail.com>
2020-08-20 13:42:11 +0300

    PORT CHANGED

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Navbar2 from './Navbar2' //<Navbar />
 import Slider from './Slider'; // <Slider />
 
 function App() {
+  console.log(process.env.PORT);
   return (
     <div className="App">
       <Navbar2 />
@@ -12,3 +13,8 @@ function App() {
 }
 
 export default App;
+
+
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
